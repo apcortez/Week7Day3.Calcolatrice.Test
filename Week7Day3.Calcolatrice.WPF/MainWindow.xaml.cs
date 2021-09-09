2a1237aa758449d9f593beb7d660e52a3e3fb398
Angelica Cortez <angelica.cortez@hotmail.it>
2021-09-09 15:02:10 +0200

    aggiunta wps

---
+++ Week7Day3.Calcolatrice.WPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wee7Day3.Calcolatrice.Core;
+
+namespace Week7Day3.Calcolatrice.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double valueA;
+        private double valueB;
+        private string operation;
+        Calculator calculator = new Calculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
